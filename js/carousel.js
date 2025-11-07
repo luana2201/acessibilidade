04162c1c41dabe975b0a0876c520eacb65419581
luana2201 <souza.siqueira.luana@escola.pr.gov.br>
2025-11-07 08:39:52 -0300

    carousel.js

---
+++ js/carousel.js
@@ -0,0 +1,15 @@
+var btns = document.querySelectorAll('.listaDeArtigo-slider-item');
+
+//percorrer todos os botoes controladores
+btns.forEach(function(btn) {
+    btn.addEventListener('click', function()) {
+
+
+        //Remover class 'ativo' dos outros botoes
+        btns.forEach(function(btnRemoveClass) {
+            btnRemoveClass.classList.remove('listyaDeArtigos-slider-item-ativo')
+        })
+
+        this.classList.add('listaDeartigo-slider-item--ativo')
+    })
+}
